eabfc3dd0bef386100565966183aace19b845270
zhenggao cai <caizhengao1992@gmail.com>
2019-04-17 06:18:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,10 +4,10 @@ def square_array(array)
   new_array=[]
   array.each do |number|
 
-  
+end
     n=0
     for n in (0..array.size-1) do 
-      new_array[n]=number*number
+      new_array[n]=array[n]*array[n]
       n=+ 1
     end
     return new_array
